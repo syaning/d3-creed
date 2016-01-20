3cb106e0ec2adbfe99135cc368a857c05350d1f2
syaning <syaningv@gmail.com>
2016-01-20 17:21:14 +0800

    new dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+var webpack = require('webpack');
+
 module.exports = {
-	entry: './index.js',
-	output: {
-		path: __dirname + '/dist',
-		filename: 'creed.js',
-		library: 'creed',
-		libraryTarget: 'umd'
-	},
-	watch: true,
-	externals: {
-		d3: true
-	}
+    entry: {
+        'creed': './index.js',
+        'creed.min': './index.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js',
+        library: 'creed',
+        libraryTarget: 'umd'
+    },
+    watch: true,
+    externals: {
+        d3: true
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        })
+    ]
 };
